03f865506ef9d2669860789b622d8ff06440b132
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2021-06-10 17:40:37 +0700

    Add files via upload

---
+++ Sudoku_Main.py
@@ -0,0 +1,15 @@
+from Sudoku_DFS import solve
+
+print ("\n\nSolving Sudoku 6x6...")
+board = [[0,6,0,0,0,0],
+      [0,0,3,0,0,0],
+      [0,0,0,0,0,1],
+      [0,0,0,3,0,0],
+      [0,0,0,0,6,0],
+      [0,0,0,0,0,0]]
+
+print ("Board:")
+for row in board:
+      print (row)
+
+solve(board)
